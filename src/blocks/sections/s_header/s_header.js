a9f6903c6c20db4f89d70a2562f60935d05a6224
Bochkarev Ivan <ivanx86@gmail.com>
2023-05-25 11:22:44 +0600

    feat(component) Доработана шапка

--- src/blocks/sections/s_header/s_header.js
+++ src/blocks/sections/s_header/s_header.js
@@ -4,27 +4,35 @@ Swiper.use([Pagination, Navigation])
 
 let header = document.querySelector('.s-header'),
   headerBurger = document.querySelector('.s-header__burger'),
-  headerNavigation = document.querySelector('.s-header__nav'),
-  backdrop = document.querySelector('.backdrop'),
+  mobileModalClose = document.querySelector('.m-modal-menu__close'),
+  headerNavigation = document.querySelector('.m-modal-menu'),
+  // backdrop = document.querySelector('.backdrop'),
   breakpointScroll = document.getElementsByClassName('s-header')['0'].offsetHeight,
   bodyBlock = document.querySelector('body')
 
 document.addEventListener('DOMContentLoaded', function () {
   // Открытие/Закрытие мега меню по клику на бургер
   headerBurger.addEventListener('click', () => {
-    headerBurger.classList.toggle('is-open')
+    // headerBurger.classList.toggle('is-open')
     headerNavigation.classList.toggle('is-show')
-    backdrop.classList.toggle('is-active')
+    // backdrop.classList.toggle('is-active')
+    bodyBlock.classList.toggle('is-blocked')
+  })
+
+  mobileModalClose.addEventListener('click', () => {
+    // headerBurger.classList.toggle('is-open')
+    headerNavigation.classList.toggle('is-show')
+    // backdrop.classList.toggle('is-active')
     bodyBlock.classList.toggle('is-blocked')
   })
 
   // Закрытие меню по клику вне меню
-  backdrop.addEventListener('click', () => {
-    headerBurger.classList.remove('is-open')
-    backdrop.classList.remove('is-active')
-    bodyBlock.classList.remove('is-blocked')
-    headerNavigation.classList.remove('is-show')
-  })
+  // backdrop.addEventListener('click', () => {
+  //   headerBurger.classList.remove('is-open')
+  //   backdrop.classList.remove('is-active')
+  //   bodyBlock.classList.remove('is-blocked')
+  //   headerNavigation.classList.remove('is-show')
+  // })
 
   // Фиксация шапки при скроле
   document.addEventListener('scroll', () => {

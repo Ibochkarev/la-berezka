b06fc7819a2c044d29e496240dc4f9e17e4c3e0d
Bochkarev Ivan <ivanx86@gmail.com>
2023-05-25 12:22:32 +0600

    feat(component) Добавлена панель с поиском

--- src/blocks/sections/s_header/s_header.js
+++ src/blocks/sections/s_header/s_header.js
@@ -4,35 +4,46 @@ Swiper.use([Pagination, Navigation])
 
 let header = document.querySelector('.s-header'),
   headerBurger = document.querySelector('.s-header__burger'),
-  mobileModalClose = document.querySelector('.m-modal-menu__close'),
+  headerSearchButton = document.querySelector('.s-header__actions-search'),
+  mobileModalMenuClose = document.querySelector('.m-modal-menu__close'),
+  mobileModalSearchClose = document.querySelector('.m-modal-search__close'),
   headerNavigation = document.querySelector('.m-modal-menu'),
-  // backdrop = document.querySelector('.backdrop'),
+  mobileModalSearch = document.querySelector('.m-modal-search'),
+  backdrop = document.querySelector('.backdrop'),
   breakpointScroll = document.getElementsByClassName('s-header')['0'].offsetHeight,
   bodyBlock = document.querySelector('body')
 
 document.addEventListener('DOMContentLoaded', function () {
   // Открытие/Закрытие мега меню по клику на бургер
   headerBurger.addEventListener('click', () => {
-    // headerBurger.classList.toggle('is-open')
     headerNavigation.classList.toggle('is-show')
-    // backdrop.classList.toggle('is-active')
     bodyBlock.classList.toggle('is-blocked')
   })
 
-  mobileModalClose.addEventListener('click', () => {
-    // headerBurger.classList.toggle('is-open')
+  mobileModalMenuClose.addEventListener('click', () => {
     headerNavigation.classList.toggle('is-show')
-    // backdrop.classList.toggle('is-active')
     bodyBlock.classList.toggle('is-blocked')
   })
 
-  // Закрытие меню по клику вне меню
-  // backdrop.addEventListener('click', () => {
-  //   headerBurger.classList.remove('is-open')
-  //   backdrop.classList.remove('is-active')
-  //   bodyBlock.classList.remove('is-blocked')
-  //   headerNavigation.classList.remove('is-show')
-  // })
+  // Открытие/Закрытие модалки поиска по клику на кнопку поиска
+  headerSearchButton.addEventListener('click', () => {
+    mobileModalSearch.classList.toggle('is-show')
+    backdrop.classList.toggle('is-active')
+    bodyBlock.classList.toggle('is-blocked')
+  })
+
+  mobileModalSearchClose.addEventListener('click', () => {
+    mobileModalSearch.classList.toggle('is-show')
+    backdrop.classList.toggle('is-active')
+    bodyBlock.classList.toggle('is-blocked')
+  })
+
+  // Закрытие модалки поиска по клику вне меню
+  backdrop.addEventListener('click', () => {
+    mobileModalSearch.classList.remove('is-show')
+    backdrop.classList.remove('is-active')
+    bodyBlock.classList.remove('is-blocked')
+  })
 
   // Фиксация шапки при скроле
   document.addEventListener('scroll', () => {

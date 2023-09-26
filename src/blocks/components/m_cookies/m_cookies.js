1c9762514c7013088331e2c818fc0a331f08f0d8
Ivan Bochkarev <ibochkarev@uma.tech>
2023-09-26 10:27:37 +0600

    feat(component) обновление плашки с куками

--- src/blocks/components/m_cookies/m_cookies.js
+++ src/blocks/components/m_cookies/m_cookies.js
@@ -4,6 +4,7 @@ window.addEventListener(
     if (!Cookies.get('acceptCookies')) {
       document.querySelector('.m-cookies').classList.add('is-show')
     }
+
     document.querySelector('.m-cookies__button--agree').addEventListener('click', () => {
       document.querySelector('.m-cookies').classList.remove('is-show')
       setTimeout(function () {
@@ -12,6 +13,11 @@ window.addEventListener(
           path: '/',
         })
       })
+    })
+
+    document.querySelector('.m-cookies__close').addEventListener('click', () => {
+      document.querySelector('.m-cookies__close').classList.toggle('is-open')
+      document.querySelector('.m-cookies__wrapper').classList.toggle('is-active')
     })
   },
   false,

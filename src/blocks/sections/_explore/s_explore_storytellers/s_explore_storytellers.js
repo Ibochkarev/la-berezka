e85b3a612463efc67e740ef0ef22fb33e74ed4ea
Ivan Bochkarev <ibochkarev@uma.tech>
2024-05-04 11:15:06 +0600

    feat(component) Обновление шаблонов

--- src/blocks/sections/_explore/s_explore_storytellers/s_explore_storytellers.js
+++ src/blocks/sections/_explore/s_explore_storytellers/s_explore_storytellers.js
@@ -1,14 +1,22 @@
-import { Swiper, Navigation } from 'swiper'
-Swiper.use([Navigation])
+import { Swiper, Navigation, Pagination, EffectFade } from 'swiper'
+Swiper.use([Navigation, Pagination, EffectFade])
 
 window.addEventListener(
   'load',
   () => {
     const exploreStorytellersSlider = new Swiper(`.s-explore-storytellers__slider`, {
       slidesPerView: 1,
+      effect: 'fade',
       navigation: {
         nextEl: '.s-explore-storytellers__button-next',
         prevEl: '.s-explore-storytellers__button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+        renderFraction: function (currentClass, totalClass) {
+          return '<span class="' + currentClass + '"></span>' + ' / ' + '<span class="' + totalClass + '"></span>'
+        },
       },
       resistance: true,
       resistanceRatio: 1,
@@ -17,15 +25,12 @@ window.addEventListener(
       autoHeight: true,
       breakpoints: {
         992: {
-          slidesPerView: 1,
           spaceBetween: 28,
         },
         1024: {
-          slidesPerView: 1,
           spaceBetween: 0,
         },
         1336: {
-          slidesPerView: 1,
           spaceBetween: 136,
         },
       },

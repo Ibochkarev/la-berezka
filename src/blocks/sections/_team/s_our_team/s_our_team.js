72c5a1b1c483fcb06c9f4ef7288a845710a0e7c6
Ivan Bochkarev <ibochkarev@uma.tech>
2024-06-19 11:03:28 +0600

    feat(component) Обновление шаблонов

--- src/blocks/sections/_team/s_our_team/s_our_team.js
+++ src/blocks/sections/_team/s_our_team/s_our_team.js
@@ -1,38 +1,40 @@
 import { Swiper, Navigation, Thumbs } from 'swiper'
 Swiper.use([Navigation, Thumbs])
 
-window.addEventListener(
-  'load',
-  () => {
-    const ourTeamItemsSlider = new Swiper(`.s-our-team__slider`, {
-      slidesPerView: 1,
-      resistance: true,
-      resistanceRatio: 1,
-      spaceBetween: 0,
-      autoHeight: true,
-    })
-    const ourTeamItemsSliderBottom = new Swiper(`.s-our-team__bottom`, {
-      slidesPerView: 1,
-      resistance: true,
-      resistanceRatio: 1,
-      spaceBetween: 0,
-      navigation: {
-        nextEl: '.s-our-team__button-next',
-        prevEl: '.s-our-team__button-prev',
-      },
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 0,
+if (document.querySelector('.s-our-team__slider')) {
+  window.addEventListener(
+    'load',
+    () => {
+      const ourTeamItemsSlider = new Swiper(`.s-our-team__slider`, {
+        slidesPerView: 1,
+        resistance: true,
+        resistanceRatio: 1,
+        spaceBetween: 0,
+        autoHeight: true,
+      })
+      const ourTeamItemsSliderBottom = new Swiper(`.s-our-team__bottom`, {
+        slidesPerView: 1,
+        resistance: true,
+        resistanceRatio: 1,
+        spaceBetween: 0,
+        navigation: {
+          nextEl: '.s-our-team__button-next',
+          prevEl: '.s-our-team__button-prev',
         },
-      },
-      thumbs: {
-        swiper: ourTeamItemsSlider,
-      },
-    })
+        breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+          },
+        },
+        thumbs: {
+          swiper: ourTeamItemsSlider,
+        },
+      })
 
-    ourTeamItemsSlider.controller.control = ourTeamItemsSliderBottom
-    ourTeamItemsSliderBottom.controller.control = ourTeamItemsSlider
-  },
-  false,
-)
+      ourTeamItemsSlider.controller.control = ourTeamItemsSliderBottom
+      ourTeamItemsSliderBottom.controller.control = ourTeamItemsSlider
+    },
+    false,
+  )
+}
